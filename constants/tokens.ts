f159017e1880abd126754d28fb235344780de28d
imbrickiy <imbrickiy@ya.ru>
2025-02-20 18:32:41 +0300

    стилизован TabBar и добавлен Blur эффект

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -1,11 +1,12 @@
 export const colors = {
   background: "#F8F8F8",
+  backgroundTabBar: "#FFFFFF",
   primary: "#121536",
   text: "#191b28",
 };
 
 export const fontSize = {
-  xs: 12,
+  xs: 10,
   sm: 14,
   base: 18,
   lg: 22,
--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,12 +1,41 @@
-import { Tabs } from "expo-router"
+import { colors, fontSize } from "constants/tokens";
+import { BlurView } from "expo-blur";
+import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
 
 const TabsNavigation = () => {
-    return <Tabs>
-        <Tabs.Screen name='wishlist'/>
-        <Tabs.Screen name='library'/>
-        <Tabs.Screen name='(songs)'/>
-        <Tabs.Screen name='artists'/>
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: fontSize.xs,
+          fontWeight: "600",
+        },
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          borderTopWidth: 0,
+          paddingTop: 0,
+        },
+        tabBarBackground: () => (
+          <BlurView
+            intensity={10}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: "hidden",
+              backgroundColor: colors.backgroundTabBar,
+            }}
+          />
+        ),
+      }}
+    >
+      <Tabs.Screen name="wishlist" />
+      <Tabs.Screen name="library" />
+      <Tabs.Screen name="(songs)" />
+      <Tabs.Screen name="artists" />
     </Tabs>
-}
+  );
+};
 
-export default TabsNavigation
+export default TabsNavigation;
